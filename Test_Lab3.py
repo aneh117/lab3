b8b5b99b424405035921db423e09a4acc9251238
ani <anirudh77.23@ichat.sp.edu.sg>
2024-05-23 14:05:08 +0800

    enter

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -24,7 +24,16 @@ def test_bubble_sort_descending():
 def test_bubble_sort_invalid():
     result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
+    result = Lab3.bubble_sort(input_arr,3)
+    assert (result == 0)
 
-    result = Lab3.bubble_sort(input_arr, 3)
-
-    assert (result == [])
+def test_bubble_ten():
+    result = []
+    input_arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+    result =Lab3.bubble_sort(input_arr,3)
+    assert (result == 1)
+def test_bubble_zero():
+    result = []
+    input_arr = []
+    result= Lab3.bubble_sort(input_arr,4)
+    assert (result == 0)
